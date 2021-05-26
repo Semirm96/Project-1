180c9c9ab31e1048d8fce93d55052c1938d4f469
Franklyn Diaz <franklyn_diaz@hotmail.com>
2021-05-26 11:14:57 -0400

    Adding  comments and links used for information to JS and CSS code Fixing main page typo, UPdated menu colors, Added background color of HTML body(whole page background)

--- assets/js/script.js
+++ assets/js/script.js
@@ -9,10 +9,11 @@ navigation function to add meny ot all pages
 function insertMenu() {
 
     $("#menu-placeholder").load("nav.html", function () {
-        // set the active menu item based on the name of the page
+        // After the html load is complete we can work with the navigation items
+        // Set the active menu item based on the name of the page
         var fileName = location.pathname.split("/").slice(-1)[0].split(".")[0];
-        // using getElementByID as shown in class
-        //https://api.jquery.com/load/ this gies an example of load with promise
+        //Using $() query as shown in class
+        //https://api.jquery.com/load/ this gives an example of load with promise
         $("#" + fileName + "menu")[0].classList.add("active");
 
     });
