6312fdb467df3887ca74e407510de34c28127e96
Franklyn Diaz <franklyn_diaz@hotmail.com>
2021-05-25 22:41:46 -0400

    eliminating extra comments

--- assets/js/script.js
+++ assets/js/script.js
@@ -7,17 +7,17 @@ navigation function to add meny ot all pages
 */
 
 function insertMenu() {
-    
+
     $("#menu-placeholder").load("nav.html", function () {
-      // set the active menu item based on the name of the page
-    var fileName = location.pathname.split("/").slice(-1)[0].split(".")[0];
-    // using getElementByID as shown in class
-    //https://api.jquery.com/load/ this gies an example of load with promise
-    $("#" + fileName+ "menu")[0].classList.add("active");       
+        // set the active menu item based on the name of the page
+        var fileName = location.pathname.split("/").slice(-1)[0].split(".")[0];
+        // using getElementByID as shown in class
+        //https://api.jquery.com/load/ this gies an example of load with promise
+        $("#" + fileName + "menu")[0].classList.add("active");
 
     });
 
 };
-  
+
 insertMenu();
 
