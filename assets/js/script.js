4c7059a228c7576b6a6105694ac8d0ec4b70a2f8
Franklyn Diaz <franklyn_diaz@hotmail.com>
2021-05-25 22:34:26 -0400

    Adding menu in separateHTML to use in all pages, fixing import of css using stylesheet, adding menu to all html pages with CSS and JS to match

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,3 +1,23 @@
 /*
 Main script to handle restaurant information
 */
+
+/*
+navigation function to add meny ot all pages
+*/
+
+function insertMenu() {
+    
+    $("#menu-placeholder").load("nav.html", function () {
+      // set the active menu item based on the name of the page
+    var fileName = location.pathname.split("/").slice(-1)[0].split(".")[0];
+    // using getElementByID as shown in class
+    //https://api.jquery.com/load/ this gies an example of load with promise
+    $("#" + fileName+ "menu")[0].classList.add("active");       
+
+    });
+
+};
+  
+insertMenu();
+
