60d549d039e6515f6cd9e0b4587670bb51dcbaae
Franklyn Diaz <franklyn_diaz@hotmail.com>
2021-06-05 10:34:13 -0400

    adding profile page entries, enabling readonly feature of user info

---
+++ assets/js/profile.js
@@ -0,0 +1,20 @@
+/* 
+profile page functionality
+*/
+
+const nameIn = ('#nameinput');
+const nameIn = ('#nameinput');
+const nameIn = ('#nameinput');
+const nameIn = ('#nameinput');
+
+const nameIn = ('#nameinput');
+
+
+
+function editUserData() {
+    $("#").removeAttribute('readonly');
+}
+
+function saveUserData() {
+
+}
