fe688d45a10fefd6a4c32e9968449538d992580f
Franklyn Diaz <franklyn_diaz@hotmail.com>
2021-06-06 21:17:21 -0400

    change to use new GUI for signup and login

---
+++ assets/js/login.js
@@ -0,0 +1,47 @@
+/*
+ Script to handle signup and login
+ Save/retrieve user login info and profiles
+*/
+
+const loginButton = document.getElementById("loginComp")
+const loginInvalid = document.getElementById("loginMsg")
+
+function verifyLogin(id, pswd) {
+    let accounts = getFromSession("useraccounts");
+
+    if ( (accounts === null) || (accounts.lenght === 0)) {
+        return false;
+    }
+
+    for (let i = 0; i < accounts.length; i++) {
+        if (accounts[i].user === id && accounts[i].pswd === pswd) {
+            // set the userID only as value for loggedIN in sessionStorage
+            setToSession("loggedIn", accounts[i].user);
+            return true;
+        }
+    };
+
+    // no user logged in report it in menu area
+    return false;
+}
+
+loginButton.addEventListener("click", (e) => {
+    e.preventDefault();
+    let uid = document.getElementById("user").value
+    let uidPwd = document.getElementById("psw").value
+    if (verifyLogin(uid, uidPwd)) {
+
+        //need to update with final page location for succesful login
+        // this location is the profile page
+        window.location.assign("profile.html")
+        return true;
+    } else {
+        
+       // alert("Login was unsuccessful, please register or check your username and password");
+        //loginInvalid.css("color","red");
+        loginInvalid.innerText = "invalid login, please try again....";
+        
+        setTimeout(function(){ window.location.assign("index.html") }, 2000);
+        return false;
+    }
+});
---
+++ assets/js/signup.js
@@ -0,0 +1,126 @@
+
+/*
+document.getElementById("button").addEventListener("click", createAccnt);
+*/
+
+function isUserRegistered(user) {
+    var found = false;
+    for(var i = 0; i < userAccounts.length; i++) {
+        if (userAccounts[i].user == user) {
+            found = true;
+            break;
+        }
+    }
+    return found;
+}
+
+function createAccnt() {
+
+    // create var for name, d.o.b, email, phone number, address
+    const fullName = document.getElementById("name").value
+    const user = document.getElementById("user").value
+    const email = document.getElementById("email").value
+    const pwd = document.getElementById("psw").value
+    const confPwd = document.getElementById("conPsw").value
+
+    let valid = true;
+    let tmppUser = user;
+    //Password constraints 
+    /* regular expression for different cases
+    https://stackoverflow.com/questions/19605150/regex-for-password-must-contain-at-least-eight-characters-at-least-one-number-a
+    */
+    // Minimum eight characters, at least one letter and one number:
+    let pswExp = /(a-z,A-Z,0-9_)(a-zA-Z0-9\-)(?=.*? [# ? !@$%^&* -])/;
+    /*/^(?=.*[A-Za-z])(?=.*\d)[A-Za-z\d]{8,}$/;*/
+
+    let letters = /[a-zA-Z0-9_][a-zA-Z0-9_ ]*[a-zA-Z0-9_]$/;
+    let charec = /(a-z,A-Z,0-9_)(a-zA-Z0-9\-)(?=.*?[#?!@$%^&*-])/;
+
+/*
+    if (fullName == "") {
+        alert('Name needed');
+        valid = false;
+    }
+    else if (!letters.test(fullName)) {
+        alert('Name field required only alphabet characters');
+        valid = false;
+    }
+
+    if (user == "") {
+        alert('Username needed');
+        valid = false;
+    }
+    else if (!charec.test(user)) {
+        alert('Username field required only alphabet characters');
+        valid = false;
+    }
+
+    if (email == "") {
+        alert('Email needed');
+        valid = false;
+    }
+    else if (!letter.test(email)) {
+        alert(' Email Invalid ');
+        valid = false;
+    }
+
+    else if (pwd == '') {
+        alert('Please enter Password');
+        valid = false;
+    }
+    else if (confPwd == '') {
+        alert('Enter Confirm Password');
+        valid = false;
+    }
+    else if (!pswExp.test(pwd)) {
+        alert('Password does not meet criteria. Upper case, Lower case, Special character and Numeric letter are required in Password filed');
+        valid = false;
+    }
+    else if (pwd != confPwd) {
+        alert('Passwords do not match');
+        valid = false;
+    }
+
+    if (document.getElementById("pws").value.length < 8) {
+        alert('Password minimum length is 8');
+        valid = false;
+    }
+
+    if (document.getElementById("pws").value.length > 12) {
+        alert('Password max length is 12');
+        valid = false;
+    }
+*/
+    if (valid) {
+        
+        if (!isUserRegistered(user)) {
+            addUser(fullName, user, email, pwd);
+            // print success and, after a few second, redirect to login
+            $("#signupMsg").css("color","blue");;
+            $("#signupMsg").text("Sign up successful.  Welcome " + fullName + "!!!.  Redirecting to login...");
+            
+            setTimeout(function(){ window.location.assign("index.html") }, 3000);
+        } else {
+            //alert("User already registered.");
+            // print success and, after a few second, redirect to login
+            $("#signupMsg").attr("color","red");
+            $("#signupMsg").text("User already registered.");
+            
+            setTimeout(function(){ window.location.assign("signup.html") }, 3000);
+        }
+    }
+}
+
+//store account using standard functions from storage.js
+/*
+function store() {
+    let usr = user;
+
+    //the account should be an object see storage.js
+    localStorage.setItem("user");
+    localStorage.setItem("pwd");
+    localStorage.setItem("fullName")
+    localStorage.setItem("confPwd")
+    localStorage.setItem("email")
+}
+*/
