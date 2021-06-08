0e8526e3b58882d79e1a23495b7d96abc6cec268
Franklyn Diaz <franklyn_diaz@hotmail.com>
2021-06-08 14:59:15 -0400

    using my REGEX expressions tested in regex101.com

--- assets/js/signup.js
+++ assets/js/signup.js
@@ -5,7 +5,7 @@ document.getElementById("button").addEventListener("click", createAccnt);
 
 function isUserRegistered(user) {
     var found = false;
-    for(var i = 0; i < userAccounts.length; i++) {
+    for (var i = 0; i < userAccounts.length; i++) {
         if (userAccounts[i].user == user) {
             found = true;
             break;
@@ -30,83 +30,87 @@ function createAccnt() {
     https://stackoverflow.com/questions/19605150/regex-for-password-must-contain-at-least-eight-characters-at-least-one-number-a
     */
     // Minimum eight characters, at least one letter and one number:
+    /*
     let pswExp = /(a-z,A-Z,0-9_)(a-zA-Z0-9\-)(?=.*? [# ? !@$%^&* -])/;
-    /*/^(?=.*[A-Za-z])(?=.*\d)[A-Za-z\d]{8,}$/;*/
-
     let letters = /[a-zA-Z0-9_][a-zA-Z0-9_ ]*[a-zA-Z0-9_]$/;
     let charec = /(a-z,A-Z,0-9_)(a-zA-Z0-9\-)(?=.*?[#?!@$%^&*-])/;
+    */
 
-/*
-    if (fullName == "") {
-        alert('Name needed');
-        valid = false;
-    }
-    else if (!letters.test(fullName)) {
-        alert('Name field required only alphabet characters');
-        valid = false;
-    }
+    let pswExp = /^(([a-zA-Z0-9 ~!@#$%^&*-_.?]{8,})*?)$/ /* "^ (?=.*? [A - Z])(?=.*? [a - z])(?=.*? [0 - 9])(?=.*? [# ? !@$ %^&* -]).{ 8,} $" */
+    let letters = /^([a-zA-Z ]*?)$/
+    let charec = "^ (?=.*? [A - Z])(?=.*? [a - z])(?=.*? [0 - 9])(?=.*? [# ? !@$ %^&* -]).{ 8,} $"
 
-    if (user == "") {
-        alert('Username needed');
-        valid = false;
-    }
-    else if (!charec.test(user)) {
-        alert('Username field required only alphabet characters');
-        valid = false;
-    }
-
-    if (email == "") {
-        alert('Email needed');
-        valid = false;
-    }
-    else if (!letter.test(email)) {
-        alert(' Email Invalid ');
-        valid = false;
-    }
-
-    else if (pwd == '') {
-        alert('Please enter Password');
-        valid = false;
-    }
-    else if (confPwd == '') {
-        alert('Enter Confirm Password');
-        valid = false;
-    }
-    else if (!pswExp.test(pwd)) {
-        alert('Password does not meet criteria. Upper case, Lower case, Special character and Numeric letter are required in Password filed');
-        valid = false;
-    }
-    else if (pwd != confPwd) {
-        alert('Passwords do not match');
-        valid = false;
-    }
-
-    if (document.getElementById("pws").value.length < 8) {
-        alert('Password minimum length is 8');
-        valid = false;
-    }
-
-    if (document.getElementById("pws").value.length > 12) {
-        alert('Password max length is 12');
-        valid = false;
-    }
-*/
+    
+        if (fullName == "") {
+            alert('Name needed');
+            valid = false;
+        }
+        else if (!letters.test(fullName)) {
+            alert('Name field required only alphabet characters');
+            valid = false;
+        }
+    
+        if (user == "") {
+            alert('Username needed');
+            valid = false;
+        }
+        else if (!charec.test(user)) {
+            alert('Username field required only alphabet characters');
+            valid = false;
+        }
+    
+        if (email == "") {
+            alert('Email needed');
+            valid = false;
+        }
+        else if (!letter.test(email)) {
+            alert(' Email Invalid ');
+            valid = false;
+        }
+    
+        else if (pwd == '') {
+            alert('Please enter Password');
+            valid = false;
+        }
+        else if (confPwd == '') {
+            alert('Enter Confirm Password');
+            valid = false;
+        }
+        else if (!pswExp.test(pwd)) {
+            alert('Password does not meet criteria. Upper case, Lower case, Special character and Numeric letter are required in Password filed');
+            valid = false;
+        }
+        else if (pwd != confPwd) {
+            alert('Passwords do not match');
+            valid = false;
+        }
+    
+        if (document.getElementById("pws").value.length < 8) {
+            alert('Password minimum length is 8');
+            valid = false;
+        }
+    
+        if (document.getElementById("pws").value.length > 12) {
+            alert('Password max length is 12');
+            valid = false;
+        }
+    
     if (valid) {
-        
+
         if (!isUserRegistered(user)) {
             addUser(fullName, user, email, pwd);
             // print success and, after a few second, redirect to login
-            $("#signupMsg").css("color","blue");;
+            $("#signupMsg").css("color", "blue");;
             $("#signupMsg").text("Sign up successful.  Welcome " + fullName + "!!!.  Redirecting to login...");
-            
-            setTimeout(function(){ window.location.assign("index.html") }, 3000);
+
+            setTimeout(function () { window.location.assign("index.html") }, 3000);
         } else {
             //alert("User already registered.");
             // print success and, after a few second, redirect to login
-            $("#signupMsg").css("color","red");;
+            $("#signupMsg").css("color", "red");;
             $("#signupMsg").text("User already registered.");
-            
-            setTimeout(function(){ window.location.assign("signup.html") }, 3000);
+
+            setTimeout(function () { window.location.assign("signup.html") }, 3000);
         }
     }
 }
