215766951a614dd4365df42ab6e2ff2300393d19
Franklyn Diaz <73242546+fjunior1@users.noreply.github.com>
2021-06-08 22:07:38 -0400

    Update login.js
    eliminating differing commits

--- assets/js/login.js
+++ assets/js/login.js
@@ -40,10 +40,6 @@ loginButton.addEventListener("click", (e) => {
         
         loginInvalid.innerText = "Invalid login, please try again.";
         setTimeout(function(){ window.location.assign("index.html") }, 1500);
-<<<<<<< HEAD
-
-=======
->>>>>>> a1ef98ff0b156ae8c8da126ae3036bf9582f541d
         return false;
     }
 });
